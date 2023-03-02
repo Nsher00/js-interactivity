525dc637e8d1fe662e62d6e4b580fc7ae2d9a043
Nsher00 <jimmysherwood9@gmail.com>
2023-03-01 19:15:24 -0700

    Intermediate Problems done

--- index.js
+++ index.js
@@ -13,21 +13,32 @@ let addMovie =(event)=>{
   movie.appendChild(movieTitle)
   movie.appendChild(deleteBtn)
   document.querySelector('ul').appendChild(movie)
-  inputField = ''
+  inputField.value = ''
 }
 
 document.querySelector('form').addEventListener('submit', addMovie)
 
 let deleteMovie =(event)=>{
   event.target.parentNode.remove()
-  message.textContent = 'Movie Deleted!'
+  message.textContent = event.target.parentNode.childNodes[0].textContent + ' Deleted!'
+  revealMessage()
 }
 
 let crossOffMovie =(event)=>{
   event.target.classList.toggle('checked')
   if (event.target.classList.contains('checked')) {
-    message.textContent = 'Movie Watched!'
+    message.textContent = `${event.target.textContent} watched!`
   }else{
-    message.textContent = 'Movie added back!'
+    message.textContent = `${event.target.textContent} added back!`
   }
+  revealMessage()
 }
+
+let revealMessage =()=>{
+  message.classList.remove('hide')
+  let timeOut = setTimeout(()=>{
+    message.classList.add('hide')
+  }, 1000)
+}
+
+
